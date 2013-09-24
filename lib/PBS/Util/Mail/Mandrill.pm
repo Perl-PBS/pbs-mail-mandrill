2d54da5d30c61b18d29116d744f4e4527d77d424
Pedro Melo <melo@simplicidade.org>
2013-09-24 06:18:37 +0100

    Added initial skels for the Mandrill utility module
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ lib/PBS/Util/Mail/Mandrill.pm
@@ -0,0 +1,7 @@
+package PBS::Util::Mail::Mandrill;
+
+# ABSTRACT: interface to Mandrill email services
+
+use PBS::Setup::Moo;
+
+1;
---
+++ t/pbs/util/mail/mandrill.t
@@ -0,0 +1,7 @@
+#!perl
+
+use PBS::Setup::Tests;
+
+use_ok('PBS::Util::Mail::Mandrill');
+
+done_testing();
