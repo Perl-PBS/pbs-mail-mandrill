5b07988e894bf1f4008318d26933649018b1de41
Pedro Melo <melo@simplicidade.org>
2013-10-02 06:46:11 +0100

    Add code to parse a list of file on disk with recorded HTTP requests:
    This is a temporary hack to deal with a situation at the office, this
    code is going away.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/PBS/Util/Mail/Mandrill.pm
+++ lib/PBS/Util/Mail/Mandrill.pm
@@ -25,4 +25,62 @@ method parse_webhook_payload ($json, $cb) {
   return $data;
 }
 
+
+##################
+# Blacklist loader
+
+method load_email_blacklist ($path) {
+  ## FIXME: use PBS::Exception
+  Carp::confess('FATAL: to use email_blacklist, email_blacklist_path is required, must be directory')
+    unless defined $path and (!-e $path or -d _);
+
+  require Path::Tiny;
+  $path = Path::Tiny::path($path);
+
+  my %bl;
+  my $iter = $path->iterator;
+  while ($path = $iter->()) {
+    unless ($path->basename =~ qr{^[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}$}) {
+      warn "File '$path' ignored, bad filename\n";
+      next;
+    }
+
+    my $payload = _parse_payload($path);
+    unless (defined $payload) {
+      warn "File '$path' ignored, could not parse it\n";
+      next;
+    }
+
+    try {
+      $self->parse_webhook_payload(
+        $payload,
+        sub {
+          my ($ev, $msg, $ts) = @_;
+          my $addr = $msg->{email};
+          return unless defined $addr;
+
+          my $info = $bl{$addr} ||= { ts => $ts };
+          return if $info->{ts} > $ts;    # Keep only last report per addr
+
+          if    ($ev eq 'hard_bounce') { $info->{skip} = 1 }
+          elsif ($ev eq 'soft_bounce') { $info->{skip} = $ts > time() - 30 * 24 * 60 * 60 }    ## retry after 30 days
+        }
+      );
+    }
+    catch {
+      warn "File '$path' ignored, could not parse JSON: $payload ($_)\n";
+    };
+  }
+
+  return \%bl;
+}
+
+func _parse_payload ($path) {
+  my $p = eval $path->slurp_raw;    # yeah, bad idea, I know...
+  Carp::confess($@) if $@;
+
+  $p->{args}{mandrill_events};
+}
+
+
 1;
